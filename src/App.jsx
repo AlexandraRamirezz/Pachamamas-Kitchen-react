af510d34cee87b1c2b626ec1bd7518a90f688506
Alexandra <alexa.dayanna07@gmail.com>
2024-08-02 03:54:14 -0500

    feat: incomplete Home section was created

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/navbar/Navbar'
+import HeroSection from './components/herosection/HeroSection'
+import ItemListContainer from './components/itemlistcontainer/ItemListContainer'
+import Footer from './components/footer/Footer'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Navbar />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className='navbar-content'>
+        <div className='content-pages'>
+          <HeroSection />
+          <ItemListContainer greeting="Welcome to Pachamama's Kitchen! We're your gateway to authentic Peruvian cuisine with a modern twist. From classic ceviche to innovative dishes, we offer a culinary journey through Peru's diverse flavors. Come and taste the soul of Peru!" />
+          <div className='container-div-container'>
+            <figure className='container-img-description'>
+              <div className='img-description'/>
+            </figure>
+            <div className='container-description'>
+              <p className='description-message'>Pachamama's Kitchen serves a variety of authentic Peruvian dishes such as ceviche, lomo saltado, ají de gallina, and anticuchos. We also offer refreshing beverages like chicha morada and pisco sour, as well as a selection of local wines and beers.</p>
+            </div>
+          </div>
+          <Footer />
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
