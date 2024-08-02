74ba4b2213cec0e236e3140fc976b741cc3c750e
Alexandra <alexa.dayanna07@gmail.com>
2024-08-02 03:47:44 -0500

    feat: cartwidget component created

---
+++ src/components/cartwidget/CartWidget.jsx
@@ -0,0 +1,12 @@
+import './CartWidget.css'
+
+const CartWidget = () => {
+  return (
+    <div className='nav-cart'>
+      <i className="fas fa-shopping-cart"></i>
+      <p>3</p>
+    </div>
+  )
+}
+
+export default CartWidget
