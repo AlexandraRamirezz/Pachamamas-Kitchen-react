83dc8b20e9f1bdda624b08c165b52a916e7cdeb1
Alexandra <alexa.dayanna07@gmail.com>
2024-08-02 03:48:38 -0500

    feat: herosection component created

---
+++ src/components/herosection/HeroSection.jsx
@@ -0,0 +1,15 @@
+import './HeroSection.css'
+
+const HeroSection = () => {
+  return (
+    <div className='background-hero-img'>
+      <div className='background-shadow'>
+        <figure className='container-img-hero'>
+          <div className='img-hero'></div>
+        </figure>
+      </div>
+    </div>
+  )
+}
+
+export default HeroSection
