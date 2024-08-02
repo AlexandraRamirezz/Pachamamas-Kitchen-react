8aa6c1fe582b1a90abf1286670f24b45e5e506db
Alexandra <alexa.dayanna07@gmail.com>
2024-08-02 03:48:11 -0500

    feat: footer component created

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='container-footer'>
+      <p className='footer-text'>© Pachamama's Kitchen, 2024. All rights reserved.</p>
+    </div>
+  )
+}
+
+export default Footer
