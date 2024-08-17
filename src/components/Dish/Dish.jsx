10b97aecef5eefd456580add547a3f5186fc3c30
Alexandra <alexa.dayanna07@gmail.com>
2024-08-17 02:03:48 -0500

    feat: the dish cards component is created

---
+++ src/components/Dish/Dish.jsx
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom';
+import './Dish.css'
+
+const Dish = ({ dish }) => {
+  return (
+      <div key={dish.id} className="card">
+        <Link to={`/menu/dish/${dish.id}`}>
+          <img src={dish.image} alt={dish.name} />
+        </Link>
+        <h3>{dish.name}</h3>
+        <p>S/ {dish.price}</p>
+        <button type='submit'>
+          Add to cart
+        </button>
+      </div>
+  );
+};
+
+export default Dish;
