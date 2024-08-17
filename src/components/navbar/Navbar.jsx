4ebdd384eaaa61b9d75740b295140fdc97f68853
Alexandra <alexa.dayanna07@gmail.com>
2024-08-17 02:34:40 -0500

    feat: a is updated by Link to use routing in the navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,31 +1,34 @@
+import { Link } from 'react-router-dom'
 import './Navbar.css'
-import CartWidget from '../cartwidget/CartWidget'
+import CartWidget from '../CartWidget/CartWidget'
 
 const Navbar = () => {
   return (
     <nav className='navbar'>
       <div className='navbar-logo'>
         <figure className='container-logo'>
-          <div className='img-logo'></div>
+          <Link to={"/home"}>
+            <div className='img-logo'></div>
+          </Link>
           <div className='img-logo-name'></div>
         </figure>
       </div>
       <div className='navbar-links'>
         <ul>
           <li className='nav-link'>
-            <a href="#">HOME</a>
+            <Link to="/home">HOME</Link>
           </li>
           <li className='nav-link'>
-            <a href="#">ABOUT</a>
+            <Link to="/about">ABOUT</Link>
           </li>
           <li className='nav-link'>
-            <a href="#">MENU</a>
+            <Link to="/menu">MENU</Link>
           </li>
           <li className='nav-link'>
-            <a href="#">RESERVATIONS</a>
+            <Link to="/reservations">RESERVATIONS</Link>
           </li>
           <li className='nav-link'>
-            <a href="#">CONTACT US</a>
+            <Link to="/contact-us">CONTACT US</Link>
           </li>
         </ul>
       </div>
