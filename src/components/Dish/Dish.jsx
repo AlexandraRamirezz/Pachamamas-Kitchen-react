ef5901c48c380c207d87b930c2db6f66f7d5205b
Alexandra <alexa.dayanna07@gmail.com>
2024-09-05 10:30:27 -0500

    feat: dishes are added one by one from their card in menu

--- src/components/Dish/Dish.jsx
+++ src/components/Dish/Dish.jsx
@@ -1,7 +1,15 @@
-import { Link } from 'react-router-dom';
 import './Dish.css'
+import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+import { CartContext } from '../../context/CartContext/CartProvider';
 
 const Dish = ({ dish }) => {
+  const { addDishes } = useContext(CartContext);
+
+  const handleAddToCart = () => {
+    addDishes(dish, 1);
+  };
+
   return (
       <div key={dish.id} className="card">
         <Link to={`/menu/dish/${dish.id}`}>
@@ -9,9 +17,7 @@ const Dish = ({ dish }) => {
         </Link>
         <h3>{dish.name}</h3>
         <p>S/ {dish.price.toFixed(2)}</p>
-        <button type='submit'>
-          Add to cart
-        </button>
+        <button onClick={handleAddToCart}>Add to cart</button>
       </div>
   );
 };
