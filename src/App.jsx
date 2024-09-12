94a719af8be6b4f26701098437a7fcd483726d33
Alexandra <alexa.dayanna07@gmail.com>
2024-09-12 04:39:13 -0500

    feat: validated that the checkout is displayed but without the navbar or footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes, useLocation } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
@@ -11,30 +11,40 @@ import NotFound from './pages/NotFound/NotFound';
 import DishDetailContainer from './components/DishDetailContainer/DishDetailContainer';
 import CartProvider from './context/CartContext/CartProvider';
 import Cart from './components/Cart/Cart';
+import Checkout from './components/Checkout/Checkout';
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <CartProvider>
-        <BrowserRouter>
-          <Navbar />
-          <div className='content-pages'>
-            <Routes>
-              <Route path="/" element={<Navigate to="/home" />} />
-              <Route path="/home" element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/menu" element={<Menu />} />
-              <Route path="/menu/category/:categoryId" element={<Menu />} />
-              <Route path="/menu/dish/:id" element={<DishDetailContainer />} />
-              <Route path="/cart" element={<Cart />} />
-              <Route path="/reservations" element={<Reservations />} />
-              <Route path="/contact-us" element={<ContactUs />} />
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-            <Footer />
-          </div>
-        </BrowserRouter>
+        <AppContent />
       </CartProvider>
+    </BrowserRouter>
+  );
+}
+
+function AppContent() {
+  const location = useLocation();
+  const isCheckout = location.pathname === "/checkout";
+  return (
+    <>
+      {!isCheckout && <Navbar />}
+      <div className={`content-pages ${isCheckout ? 'checkout' : ''}`}>
+        <Routes>
+          <Route path="/" element={<Navigate to="/home" />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/menu/category/:categoryId" element={<Menu />} />
+          <Route path="/menu/dish/:id" element={<DishDetailContainer />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/reservations" element={<Reservations />} />
+          <Route path="/contact-us" element={<ContactUs />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        {!isCheckout && <Footer />}
+      </div>
     </>
   );
 }
